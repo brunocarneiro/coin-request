e883780b18cda39b396764bc64b5ca9d5d0a7573
Bruno Carneiro <brunoac88@gmail.com>
2018-08-16 20:04:04 -0300

    [Cloud deployment] Adding frontend cloud deploy routine

--- packages/frontend/src/constants.js
+++ packages/frontend/src/constants.js
@@ -2,9 +2,9 @@ let restApiUrl;
 let host = window.location.host;
 
 if(host.indexOf("localhost")>=0 || host.indexOf("127.0.0.1")>=0){
-  restApiUrl = "https://us-central1-faucet-giver-213520.cloudfunctions.net";
-} else {
   restApiUrl = "http://localhost:4000";
+} else {
+  restApiUrl = "https://us-central1-faucet-giver-213520.cloudfunctions.net";
 }
 
 export const REST_API_URL = restApiUrl;
